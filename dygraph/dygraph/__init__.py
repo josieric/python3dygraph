47359622ed405b5de35e15622abb9e7e878e1bb2
josieric <eric.josien@free.fr>
2019-04-19 13:09:41 +0200

    Cor ;;

--- dygraph/dygraph/__init__.py
+++ dygraph/dygraph/__init__.py
@@ -2,11 +2,11 @@
 import pkg_resources
 jspath = pkg_resources.resource_filename(__name__, "dygraph.1.0.1.js")
 f = open(jspath, "r")
-jscode = f.read()
+jscode = "<script type=\"text/javascript\">"+f.read()+"</script>"
 f.close()
 jspath = pkg_resources.resource_filename(__name__, "dygraph.addon.js")
 f = open(jspath, "r")
-jscode += f.read()
+jscode += "<script type=\"text/javascript\">"+f.read()+"</script>"
 f.close()
 
 def get_jspath():
@@ -15,18 +15,19 @@ def get_jspath():
 ## renvoi un div et script dygraph autonome
 ## P1 => DataFrame indexed by Date (Cf test.py)
 def dygraph(df, df_opts, divname = "graphdiv", includejs = True):
-  dygraph="<div id=\"" +divname+ "\" style=\"margin: 0 auto; width:auto;\"></div><script type=\"text/javascript\">"
+  dygraph="<div id=\"" +divname+ "\" style=\"margin: 0 auto; width:auto;\"></div>"
   if (includejs):
-    dygraph += "\n" + jscode
-  dygraph += "\nvar  "+divname+" = new Dygraph(document.getElementById('" +divname+ "'),"
-  dygraph += "\""+ df.to_csv(index=True,line_terminator="\\n") +"\""+ "," + str(df_opts) + ");\n</script>"
+    dygraph += jscode
+  dygraph += "<script type=\"text/javascript\">var  "+divname+" = new Dygraph(document.getElementById('" +divname+ "'),"
+  dygraph += "\""+ df.to_csv(index=True,line_terminator="\\n") +"\""+ "," + str(df_opts) + ");\n"
+  dygraph += "</script>"
   return dygraph
 
 def dygraphSS(df, df_opts, divname = "graphdiv", includejs = True):
-  dygraph="<div id=\"" +divname+ "\" style=\"margin: 0 auto; width:auto;\"></div><div id=\"" +divname+ "_sel\" align=\"center\"></div><script type=\"text/javascript\">"
+  dygraph="<div id=\"" +divname+ "\" style=\"margin: 0 auto; width:auto;\"></div><div id=\"" +divname+ "_sel\" align=\"center\"></div>"
   if (includejs):
-    dygraph += "\n" + jscode
-  dygraph += "\nvar  "+divname+" = new Dygraph(document.getElementById('" +divname+ "'),"
+    dygraph += jscode
+  dygraph += "<script type=\"text/javascript\">var  "+divname+" = new Dygraph(document.getElementById('" +divname+ "'),"
   dygraph += "\""+ df.to_csv(index=True,line_terminator="\\n") +"\""+ "," + str(df_opts) + ");\n"
   dygraph += "graph_selector('" +divname+ "_sel','" +divname+ "',"+divname+");</script>"
 
